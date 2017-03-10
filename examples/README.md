e0d232440286786fdb505be2e87e004768876d5d
Desmond <desmond@pnw3dev.com>
2017-03-10 11:03:25 -0800

    instructions and required field updates

---
+++ examples/README.md
@@ -0,0 +1,48 @@
+# Customer_Add Web API
+Host: `cloud3.evosus.com`
+
+POST: `/api/method/Customer_Add?`
+
+Parameters:
+* `CompanySN=string`
+* `ticket=string`
+
+Arguments:
+Use to populate args body parameter for Customer_Add method
+* `Name_Company: string`
+* `Name_First: string`
+* `Name_Last: string`
+* `BillTo_ContactName: string`
+* `BillTo_Address1: string`
+* `BillTo_City: string`
+* `BillTo_StateAbbr: string`
+* `BillTo_PostCode: string`
+* `BillTo_Country: string`
+* `ShipTo_ContactName: string`
+* `ShipTo_Address1: string`
+* `ShipTo_Address2: string`
+* `ShipTo_City: string`
+* `ShipTo_StateAbbr: string`
+* `ShipTo_PostCode: string`
+* `ShipTo_Country: string`
+* `PhoneNumber_Mobile1: string`
+* `PhoneNumber_Mobile2: string`
+* `PhoneNumber_Home: string`
+* `PhoneNumber_Work: string`
+* `PhoneNumber_Fax: string`
+* `EmailAddress1: string`
+* `EmailAddress2: string`
+* `Customer_TypeName: string`
+* `Lead_InterestName: string`
+* `Lead_AdvertisingCode: string`
+* `Lead_EmployeeEvosusLoginName: string`
+* `Lead_StoreName: string`
+* `Lead_Date: string`
+* `WaterTest_Integration_SystemID: string`
+* `WaterTest_Integration_CustomerID: string`
+* `DataConversion_LegacySystemID: string`
+* `AustralianBusinessNumber: string`
+
+#### Sample:
+
+[Evosus Enterprise Customer_Add JavaScript Example](https://github.com/evosus/evo-public-gist/blob/master/examples/customer_add-example.js)
--- examples/customer_add-example.js
+++ examples/customer_add-example.js
@@ -1,45 +1,46 @@
 // -----------------------------------------------
-// EVOSUS Customer_Add Web API Javascript Example
+// Evosus Customer_Add Web API Javascript Example
 // -----------------------------------------------
 var data = JSON.stringify({
     "args": {
-        "Name_Company": "Evosus Pool Company",
-        "Name_First": "John",
-        "Name_Last": "Doe",
-        "BillTo_ContactName": "Johnathan",
-        "BillTo_Address1": "123 Pool St",
-        "BillTo_City": "Vancouver",
-        "BillTo_StateAbbr": "WA",
-        "BillTo_PostCode": "98665",
-        "BillTo_Country": "USA",
-        "ShipTo_ContactName": "Johnathan",
-        "ShipTo_Address1": "123 Pool St",
-        "ShipTo_Address2": "Suite 100",
-        "ShipTo_City": "Vancouver",
-        "ShipTo_StateAbbr": "WA",
-        "ShipTo_PostCode": "98665",
-        "ShipTo_Country": "USA",
-        "PhoneNumber_Mobile1": "360-735-9510",
-        "PhoneNumber_Mobile2": "360-735-9510",
-        "PhoneNumber_Home": "360-735-9510",
-        "PhoneNumber_Work": "360-735-9510",
-        "PhoneNumber_Fax": "360-735-9510",
-        "EmailAddress1": "sales@evosus.com",
-        "EmailAddress2": "dmcmanus@evosus.com",
-        "Customer_TypeName": "Web Order Customer",
-        "Lead_InterestName": "Spas",
-        "Lead_AdvertisingCode": "AdTrack",
-        "Lead_EmployeeEvosusLoginName": "admin",
-        "Lead_StoreName": "eCommerce Store",
-        "Lead_Date": "03-08-2017",
-        "WaterTest_Integration_SystemID": "{{WaterTest_Integration_SystemID}}",
-        "WaterTest_Integration_CustomerID": "{{WaterTest_Integration_CustomerID}}",
-        "DataConversion_LegacySystemID": "{{DataConversion_LegacySystemID}}",
-        "AustralianBusinessNumber": "{{AustralianBusinessNumber}}"
+        "Name_Company": "Evosus Pool Company", // Optional
+        "Name_First": "John", // REQUIRED
+        "Name_Last": "Doe", // Optional
+        "BillTo_ContactName": "Johnathan", // Optional
+        "BillTo_Address1": "123 Pool St", // Optional
+        "BillTo_City": "Vancouver", // Optional
+        "BillTo_StateAbbr": "WA", // Optional
+        "BillTo_PostCode": "98665", // Optional
+        "BillTo_Country": "USA", // Optional
+        "ShipTo_ContactName": "Johnathan", // Optional
+        "ShipTo_Address1": "123 Pool St", // Optional
+        "ShipTo_Address2": "Suite 100", // Optional
+        "ShipTo_City": "Vancouver", // Optional
+        "ShipTo_StateAbbr": "WA", // Optional
+        "ShipTo_PostCode": "98665", // Optional
+        "ShipTo_Country": "USA", // Optional
+        "PhoneNumber_Mobile1": "360-735-9510", // Optional
+        "PhoneNumber_Mobile2": "360-735-9510", // Optional
+        "PhoneNumber_Home": "360-735-9510", // Optional
+        "PhoneNumber_Work": "360-735-9510", // Optional
+        "PhoneNumber_Fax": "360-735-9510", // Optional
+        "EmailAddress1": "sales@evosus.com", // Optional
+        "EmailAddress2": "dmcmanus@evosus.com", // Optional
+        "Customer_TypeName": "Web Order Customer", // Optional
+        "Lead_InterestName": "Spas", // Optional
+        "Lead_AdvertisingCode": "AdTrack", // Optional
+        "Lead_EmployeeEvosusLoginName": "admin", // Optional
+        "Lead_StoreName": "eCommerce Store", // Optional
+        "Lead_Date": "03-08-2017", // Optional
+        "WaterTest_Integration_SystemID": "{{WaterTest_Integration_SystemID}}", // Optional
+        "WaterTest_Integration_CustomerID": "{{WaterTest_Integration_CustomerID}}", // Optional
+        "DataConversion_LegacySystemID": "{{DataConversion_LegacySystemID}}", // Optional
+        "AustralianBusinessNumber": "{{AustralianBusinessNumber}}" // Optional
     }
 });
 // URL PARAMETERS
-var params = "CompanySN=20030908100353*989&ticket=1ca0c308-0a29-4cfc-9383-f3f6cde0e74f";
+// CompanySN && ticket
+var params = "CompanySN=20030908100353*989&ticket=1ca0c308-0a29-4cfc-9383-f3f6cde0e74f"; // Required
 // -------------------------
 // BEGIN XHR REQUEST
 // --------------------------
